0dfb62291c339b63523f367c95f3400ef2875b30
LinderJonathan <linderjonathan3@gmail.com>
2026-04-26 22:07:39 +0200

    Now TS instead of JS lol

--- frontend/app/src/main.tsx
+++ frontend/app/src/main.tsx
@@ -1,9 +1,16 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
+import App from './App.tsx'
 
-createRoot(document.getElementById('root')).render(
+createRoot
+const rootElement = (document.getElementById('root'))
+  
+if (!rootElement) {
+  throw new Error("Root element not found")
+}
+
+createRoot(rootElement).render(
   /*TODO: Investigate if BrowserRouter can be used here instead */
   <StrictMode>
     <App />
